fa70fc70b44a69293dc355525160684abe57d00b
vincent porte <vincent@neuralia.co>
2022-02-15 13:04:53 +0100

    tests in progress

--- itou/siaes/tests/tests_management_commands.py
+++ itou/siaes/tests/tests_management_commands.py
@@ -1,24 +1,22 @@
-from dateutil.relativedelta import relativedelta
-
+#from dateutil.relativedelta import relativedelta
 from django.test import TestCase
 
-class ImportSiaeManagementCommandsTest(TestCase): 
 
+class ImportSiaeManagementCommandsTest(TestCase):
     def test_get_creatable_conventions(self):
         # needs
-        # Siaes wo conventions & source 'ASP'
+        # Siaes wo conventions & source 'ASP'
 
         # if not in VUE_STRUCTURE_DF, siae.is_active must be False
 
         # if in VUE_STRUCTURE_DF
         # there must be no SiaeConvention objects for this asp_id and this kind
 
-        # if does_siae_have_an_active_convention : check convention appended in list
+        # if does_siae_have_an_active_convention : check convention appended in list
 
         # if not does_siae_have_an_active_convention:
         # if siae_key not in INACTIVE_SIAE_LIST, check raise error
         # if siae_key in INACTIVE_SIAE_LIST, check convention_end_date
         # check convention appended in list
 
-
-
+        return True
